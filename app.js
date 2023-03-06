5bff8d8817c90b0dee244059257e4439cd9b7f41
Kishor-G-Hosur <ghkishor45@gmail.com>
2023-03-06 10:28:12 +0530

    commit

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express=require('express')
 const cors=require('cors')
 const app=express()
-const PORT=4000
+
 
 //! require data models
-
+const Post=require('./models/posts')
 const User=require('./models/users')
 
 const mongoose=require('mongoose')
+
+const users = require('./models/users')
 mongoose.set('strictQuery',false)
 
 //! middlewares
@@ -19,6 +21,66 @@ app.use(cors()) //! cross origin resouse origin
 const dbURL="mongodb://localhost:27017/foodie"
 mongoose.connect(dbURL).then(()=>{
     console.log("connected to database");
+})
+app.post('/login',async(req,res)=>{
+    User.findOne({email:req.body.email},(err,userData)=>{
+        if(userData){
+            if (req.body.password == userData.password)
+             {
+                res.send({message:"login Successfull"})
+            } else {
+                res.send({message:"loging failed"})
+            }
+
+        }else{
+            res.send({message:'no account is matching with the email adress'})
+        }
+    })
+})
+app.get('/posts',async(req,res)=>{
+    try{
+        const posts=await Post.find()
+        res.send(posts)
+    }catch(err)
+    {
+        console.log(err)
+    }
+})
+app.get('/users',async(req,res)=>{
+    try{
+        const users=await User.find()
+        res.send(users)
+    }catch(err)
+    {
+        console.log(err)
+    }
+})
+app.get('/posts/:id',async(req,res)=>{
+    const {id} = req.params
+    try{
+        const singlePost=await Post.findById(id)
+        res.send(singlePost)
+    }catch(error){
+        res.send(error)
+    }
+})
+app.post('/add-posts', async(req,res)=>{
+    let postData=new Post({
+        author:req.body.author,
+        Title:req.body.Title,
+        summary:req.body.summary,
+        image:req.body.image,
+        location:req.body.location
+    })
+    try{
+        await postData.save()
+        console.log(postData)
+        res.send({message:"post added successfully"})
+
+    }catch(err){
+        res.send({message:"Faild to add"})
+
+    }
 })
 
 app.post('/signup',async(req,res)=>{
@@ -46,7 +108,7 @@ app.post('/signup',async(req,res)=>{
         }
     })
 })
-
+const PORT=4000
 app.listen(PORT,()=>{
     console.log("listening to port")
 })
